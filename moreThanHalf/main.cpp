06029dec7ea1402d24e92e309620bff8891791ba
Lingyu Li <lilingyu1991@foxmail.com>
2015-10-20 17:37:47 +0800

    Initial Commit

---
+++ moreThanHalf/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  moreThanHalf
+//
+//  Created by LiLingyu on 15/10/20.
+//  Copyright © 2015年 LiLingyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
